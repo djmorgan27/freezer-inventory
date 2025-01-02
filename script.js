4d56ea28da533a198f24d6f7473b76658a71fb6b
devan <djmorgan27@live.com>
2025-01-02 14:16:57 -0500

    revert

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inventoryContainer = document.getElementById('inventory-container');
 const form = document.getElementById('add-item-form');
 
-const API_URL = 'https://djmorgan27.github.io/freezer-inventory/api/inventory';
+const API_URL = 'http://localhost:3000/api/inventory';
 let inventory = []; // Declare at the top of the script
 
 // Fetch and display inventory
